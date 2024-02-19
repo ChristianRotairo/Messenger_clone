201c57b6ea5760e0f9224fb8da42c8d85ba2aad4
ChristianRotairo <christiankitrotairo.com>
2024-02-19 08:46:40 +0800

    messages missing

--- lib/appbar/app_bar.dart
+++ lib/appbar/app_bar.dart
@@ -2,7 +2,6 @@ import 'package:clone_mess/search_bar/searchbar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart'; // Import the google_fonts package
 
-
 class App extends StatefulWidget {
   const App({Key? key});
 
@@ -15,15 +14,17 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     // Appbar
     return Scaffold(
+       drawer: const Drawer(),
       appBar: AppBar(
         title: Text(
           'Chats',
           style: GoogleFonts.roboto(
             textStyle: const TextStyle(
-              color: Colors.white,
+              color: Colors.white,     
             ),
           ),
         ),
+        iconTheme:const IconThemeData(color: Color.fromARGB(255, 255, 255, 255)),
         backgroundColor: Colors.black87,
         actions: [
           Container(
@@ -39,8 +40,31 @@ class _AppState extends State<App> {
           ),
         ],
       ),
-      body: SearchApp(),
-      backgroundColor: Colors.black12, // Place the SearchBar widget here
+      body: const SearchApp(),
+      backgroundColor: Colors.black12,
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(
+              Icons.chat_bubble_rounded,
+            ),
+            label: 'Chats',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.video_camera_back_rounded, color: Colors.white),
+            label: 'Calls',
+          ),
+           NavigationDestination(
+            icon: Icon(Icons.electric_bolt_sharp, color: Colors.white),
+            label: 'Highlights',
+          ),
+           NavigationDestination(
+            icon: Icon(Icons.people, color: Colors.white),
+            label: 'People',
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/search_bar/searchbar.dart
+++ lib/search_bar/searchbar.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_label
-
 import 'package:flutter/material.dart';
 
 class SearchApp extends StatefulWidget {
@@ -7,30 +5,59 @@ class SearchApp extends StatefulWidget {
 
   @override
   State<SearchApp> createState() => _SearchAppState();
-}class _SearchAppState extends State<SearchApp> {
+}
+
+class _SearchAppState extends State<SearchApp> {
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin:const EdgeInsets.all(16.0), // Adjust margin as needed
-      child: TextField(
-        decoration:  InputDecoration(
-          // Search bar style
-          focusedBorder: OutlineInputBorder(
-             borderRadius: BorderRadius.circular(50.0),
-               borderSide: BorderSide(color: Colors.grey),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: TextField(
+                controller: _textEditingController,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  hintText: 'Search',
+                  hintStyle: const TextStyle(fontSize: 20.0, color: Color.fromARGB(255, 146, 135, 135)),
+                  prefixIcon: const Icon(Icons.search, color: Color.fromARGB(255, 131, 131, 131)),
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 51, 51, 51),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+                ),
+                onChanged: (value) {
+                  // Implement your search logic here
+                  
+                },
+              ),
+            ),
           ),
-          hintText: 'Search',
-         hintStyle:  const TextStyle(fontSize: 20.0, color: Color.fromARGB(255, 146, 135, 135)),
-          prefixIcon: const Icon(Icons.search,
-          color: Color.fromARGB(255, 131, 131, 131),),
-           filled: true,
-          fillColor: const Color.fromARGB(255, 51, 51, 51), // Set background color here
-                    contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
-        ),
-        
-        onChanged: (value) {
-          // Implement your search logic here
-        },
+          Expanded(
+            child: Container(
+              color: const Color.fromARGB(255, 0, 0, 0),
+              child: ListView.builder(
+                itemCount: 130, // Example item count, replace with your actual data
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                    textColor: Colors.white,
+                    // Add more ListTile properties or customize as needed
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
