3502550870633956ef6c02fbf195a3ca0b4e70d4
ChristianRotairo <christiankitrotairo.com>
2024-04-08 18:18:10 +0800

    Get Method using laravel api

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -1,15 +1,48 @@
 import 'package:dio/dio.dart';
 
-Future<List<dynamic>> fetchUsers() async {
-  try {
-    final dio = Dio();
-    final response = await dio.get('https://reqres.in/api/users?page=2');
-    if (response.statusCode == 200) {
-      return response.data['data'];
-    } else {
-      throw Exception('Failed to fetch users. StatusCode: ${response.statusCode}');
+class Vacation {
+  final String vacationName;
+  final String fromDate;
+  final String toDate;
+  final String description;
+  final int amount;
+
+
+  Vacation({
+    required this.vacationName,
+    required this.fromDate,
+    required this.toDate,
+    required this.description,
+    required this.amount,
+  });
+
+  factory Vacation.fromJson(Map<String, dynamic> json) {
+    return Vacation(
+      vacationName: json['vacation_name'],
+      fromDate: json['from_date'],
+      toDate: json['to_date'],
+      description: json['description'],
+      amount: json['amount'],
+    );
+  }
+}
+
+class ApiService {
+  final Dio dio = Dio();
+
+  Future<List<Vacation>> fetchVacations() async {
+    try {
+      final response = await dio.get('http://10.0.2.2:8000/api/travelhistory');
+      if (response.statusCode == 200) {
+        List<Vacation> vacations = (response.data as List)
+            .map((json) => Vacation.fromJson(json))
+            .toList();
+        return vacations;
+      } else {
+        throw Exception('Failed to fetch data. StatusCode: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch data');
     }
-  } catch (e) {
-    throw Exception('Failed to fetch users');
   }
 }
--- lib/mess_clone/view/messenger_app.dart
+++ lib/mess_clone/view/messenger_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../data/api.dart';
 
 class YourWidget extends StatefulWidget {
@@ -11,12 +10,13 @@ class YourWidget extends StatefulWidget {
 }
 
 class _YourWidgetState extends State<YourWidget> {
-  late Future<List<dynamic>> _futureData;
+  late Future<List<Vacation>> _futureData;
+  final ApiService _apiService = ApiService();
 
   @override
   void initState() {
     super.initState();
-    _futureData = fetchUsers();
+    _futureData = _apiService.fetchVacations();
   }
 
   @override
@@ -25,81 +25,39 @@ class _YourWidgetState extends State<YourWidget> {
       appBar: AppBar(
         title: const Text('Your Widget'),
       ),
-      body: Column(
-        children: [
-          const SearchBar(hintText: 'Search'),
-          Expanded(
-            child: FutureBuilder<List<dynamic>>(
-              future: _futureData,
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return const Center(child: CircularProgressIndicator());
-                  case ConnectionState.done:
-                    if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else {
-                      List<dynamic> data = snapshot.data ?? [];
-                      return ListView.builder(
-                        itemCount: data.length,
-                        itemBuilder: (context, index) {
-                          final item = data[index];
-                          return InkWell(
-                              onTap: () {
-                                // Handle item tap
-                              },
-                              child: ListTile(
-                                leading: CircleAvatar(
-                                  backgroundImage:
-                                      NetworkImage(item['avatar'] ?? ''),
-                                ),
-                                title: Text(
-                                  '${item['first_name']} ${item['last_name']}',
-                                  style: const TextStyle(
-                                      color: Color.fromARGB(255, 0, 0, 0)),
-                                ),
-                                subtitle: Text('${item['email']}'),
-                              ));
-                        },
-                      );
-                    }
-                  default:
-                    return const SizedBox(); // Return an empty SizedBox for other connection states
-                }
+      body: FutureBuilder<List<Vacation>>(
+        future: _futureData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError || snapshot.data == null) {
+            return Center(
+                child: Text('Error: ${snapshot.error ?? "Data is null"}'));
+          } else {
+            List<Vacation> data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final vacation = data[index];
+                return ListTile(
+                  title: Text(
+                    vacation.vacationName,
+                    style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('From: ${vacation.fromDate}'),
+                      Text('To: ${vacation.toDate}'),
+                      Text('Description: ${vacation.description}'),
+                      Text('Amount: ${vacation.amount}'), // assuming amount is an int
+                    ],
+                  ),
+                );
               },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SearchBar extends StatelessWidget {
-  final String hintText;
-
-  const SearchBar({Key? key, required this.hintText}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        style: const TextStyle(color: Colors.white), // Text color
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: const TextStyle(color: Colors.white), // Hint text color
-          filled: true,
-          fillColor: const Color.fromARGB(255, 29, 29, 29), // Background color
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(50.0),
-            borderSide: BorderSide.none, // No border side
-          ),
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-          prefixIcon:
-              const Icon(Icons.search, color: Colors.white), // Icon color
-        ),
+            );
+          }
+        },
       ),
     );
   }
